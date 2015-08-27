c3247f94860734e096f6da6cf97dc5feb63c4264
SpyrosMArtel <sm222gb@student.lnu.se>
2015-08-28 01:24:14 +0300

    Final commit...

--- TCPFit.h
+++ TCPFit.h
@@ -1,80 +1,77 @@
 #ifndef __INET_TCPFIT_H
 #define __INET_TCPFIT_H
 
-#include "inet/common/INETDefs.h"
+#include "INETDefs.h"
 
-#include "inet/transportlayer/tcp/flavours/TCPBaseAlg.h"
-#include "inet/transportlayer/tcp/flavours/TCPSegmentTransmitInfoList.h"
+#include "TCPBaseAlg.h"
+#include "TCPSegmentTransmitInfoList.h"
 
-namespace inet {
+class INET_API TCPFitStateVariables: public TCPBaseAlgStateVariables {
 
-    namespace tcp {
+public:
+    TCPFitStateVariables();
+    ~TCPFitStateVariables();
 
-        class INET_API TCPFitStateVariables : public TCPBaseAlgStateVariables {
+    virtual std::string info() const override;
+    virtual std::string detailedInfo() const override;
 
-        public:
-            TCPFitStateVariables();
-            ~TCPFitStateVariables();
+    TCPSegmentTransmitInfoList regions;
 
-            virtual std::string info() const override;
-            virtual std::string detailedInfo() const override;
+    double alpha; /* Additive increase */
+    double beta; /* Muliplicative decrease */
+    double nValue; /* tcp-fit n parameter */
+    double cwnd_cnt; /* Congestion window counter. */
+    simtime_t upd_interval; /* interval used for updating N */
+    simtime_t w_RTTmin; /* min RTT */
+    simtime_t w_RTTmax; /* max RTT */
+    simtime_t epoch_start; /* when an update occurred  */
+    simtime_t update_epoch; /* update interval for N */
+    simtime_t RTT_cnt; /* rtt counter */
+    simtime_t avgRTT; /* average Round Trip time; */
+    simtime_t w_lastAckTime;    // last received ack time
+    uint32 ACK_cnt; /* ACK counter */
+    uint32 ssthresh; /* < slow start threshold */
 
-            uint32 ssthresh;        /* < slow start threshold */
-            simtime_t w_RTTmin;     /* min RTT */
-            simtime_t epoch_start;  /* when an update occurred  */
-            simtime_t update_epoch; /* update interval for N */
-            double alpha;           /* Additive increase */
-            double beta;            /* Muliplicative decrease */
-            double n;               /* tcp-fit n parameter */
-            double cwnd_cnt;        /* Congestion window counter. */
-            simtime_t RTT_cnt;      /* rtt counter */
-            uint32 ACK_cnt;         /* ACK counter */
-            simtime_t avgRTT;       /* average Round Trip time; */
+private:
+};
 
-            simtime_t w_lastAckTime;    // last received ack time
+class TCPFit: public TCPBaseAlg {
+protected:
+    cOutVector *nValueVector = new cOutVector("N Value");
+    TCPFitStateVariables *& state;    // alias to TCPFit algorithm's 'state'
 
-            TCPSegmentTransmitInfoList regions;
+    /** Create and return a TCPFitStateVariables object. */
+    virtual TCPStateVariables *createStateVariables() override {
+        return new TCPFitStateVariables();
+    }
 
-        private:
-        };
+    /** Utility function to recalculate ssthresh */
+    virtual void recalculateSlowStartThreshold();
+    /** Redefine what should happen on retransmission */
+    virtual void processRexmitTimer(TCPEventCode& event) override;
 
-        class TCPFit : public TCPBaseAlg {
-        protected:
-            TCPFitStateVariables *& state;    // alias to TCPFit algorithm's 'state'
+public:
+    /** Ctor */
+    TCPFit();
+    /* Dtor */
+    ~TCPFit();
 
-          /** Create and return a TCPFitStateVariables object. */
-          virtual TCPStateVariables *createStateVariables() override {
-              return new TCPFitStateVariables();
-          }
+    /** Redefine what should happen when data got acked, to add congestion window management */
+    virtual void receivedDataAck(uint32 firstSeqAcked) override;
 
-          /** Utility function to recalculate ssthresh */
-          virtual void recalculateSlowStartThreshold();
-          /** Redefine what should happen on retransmission */
-          virtual void processRexmitTimer(TCPEventCode& event) override;
+    /** Redefine what should happen when dupAck was received, to add congestion window management */
+    virtual void receivedDuplicateAck() override;
 
-        public:
-          /** Ctor */
-          TCPFit();
+    /** Called after we send data */
+    virtual void dataSent(uint32 fromseq) override;
 
-          /** Redefine what should happen when data got acked, to add congestion window management */
-          virtual void receivedDataAck(uint32 firstSeqAcked) override;
+    virtual void segmentRetransmitted(uint32 fromseq, uint32 toseq) override;
 
-          /** Redefine what should happen when dupAck was received, to add congestion window management */
-          virtual void receivedDuplicateAck() override;
+    virtual void tcpFitUpdateN();
 
-          /** Called after we send data */
-          virtual void dataSent(uint32 fromseq) override;
+private:
+    simtime_t currentTime; // current time in simulation
 
-          virtual void segmentRetransmitted(uint32 fromseq, uint32 toseq) override;
-
-          virtual void tcpFitUpdateN();
-
-        private:
-          simtime_t currentTime; // current time in simulation
-
-          virtual void tcpFitReset();
-        };
-
-    } // namespace tcp
-} // namespace inet
+    virtual void tcpFitReset();
+};
 #endif
